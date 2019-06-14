9e362cbb1acfe451eebd50bd36c8c5df6c0428c1
TuanNguyen27 <anhtuan277@gmail.com>
2019-06-13 23:18:13 -0700

    fix syntax error

--- vq_vae/main.py
+++ vq_vae/main.py
@@ -144,8 +144,8 @@ def train(epoch, model, train_loader, optimizer, cuda, log_interval, save_path, 
         loss.backward()
         optimizer.step()
         latest_losses = model.latest_losses()
-        full_mse_losses.append((latest_losses['full_mse'])
-    return torch.cat(full_mse_losses).reshape(-1)
+        full_mse_losses.append(latest_losses['full_mse'])
+    return torch.stack(full_mse_losses).reshape(-1)
 
         # for key in latest_losses:
         #     losses[key + '_train'] += float(latest_losses[key])
@@ -195,8 +195,8 @@ def test_net(epoch, model, test_loader, cuda, save_path, args):
             model.loss_function(data, *outputs)
             latest_losses = model.latest_losses()
             full_mse_losses.append(latest_losses['full_mse'])
-    print(torch.cat(full_mse_losses).reshape(-1).size())
-    return torch.cat(full_mse_losses).reshape(-1)
+    print(torch.stack(full_mse_losses).reshape(-1).size())
+    return torch.stack(full_mse_losses).reshape(-1)
             # for key in latest_losses:
             #     losses[key + '_test'] += float(latest_losses[key])
             # if i == 0:
